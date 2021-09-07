cdd103e1c92261fa4bf869575af652c02044a44f
Eric Yan <yanxuok@gmail.com>
2021-09-07 19:26:34 +0800

    Ha I'm back for coding

---
+++ array/two_pointer_swap_or_assignment/remove_duplicates.py
@@ -0,0 +1,22 @@
+class RemoveDuplicates:
+    def __init__(self):
+        """
+        Given a sorted array, remove the duplicates in-place
+        such that each element appear only once and return the new length.
+
+        * - 限制条件：
+        *   1. Do not allocate extra space.
+        *   2. 结果可以是将 remove 掉的元素放在数组尾部，不需要硬删除。
+        *   3. 除了被 remove 掉的元素之外，其他元素的相对顺序不能变。
+
+        """
+        pass
+
+    def run(self):
+        print(self.__class__.__name__)
+
+
+
+if __name__ == '__main__':
+    o = RemoveDuplicates()
+    o.run()
