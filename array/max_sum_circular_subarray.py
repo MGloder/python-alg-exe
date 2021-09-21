edec9e9cd03b9b4b7fee1cee148a4a58225a5b55
Eric Yan <yanxuok@gmail.com>
2021-09-21 11:06:19 +0800

    remove unnecessary code

--- array/max_sum_circular_subarray.py
+++ array/max_sum_circular_subarray.py
@@ -1,25 +1,14 @@
 class Solution:
-    def getMaxResult(self, start_index, direction, nums: List[int], current_step) -> int:
-        if direction < 0:
-            if current_step > 1:
-                if start_index > 0 and start_index <= len(nums) - 1:
-                    return max(nums[start_index],
-                               nums[start_index] +
-                               self.getMaxResult(start_index - 1, -1, nums, current_step - 1))
-                else:
-                    return nums[start_index]
+    def getMaxResult(self, start_index, nums: List[int], current_step) -> int:
+        if current_step > 1:
+            if start_index >= 0 and start_index < len(nums) - 1:
+                return max(nums[start_index],
+                           nums[start_index] +
+                           self.getMaxResult(start_index + 1, 1, nums, current_step - 1))
             else:
                 return nums[start_index]
         else:
-            if current_step > 1:
-                if start_index >= 0 and start_index < len(nums) - 1:
-                    return max(nums[start_index],
-                               nums[start_index] +
-                               self.getMaxResult(start_index + 1, 1, nums, current_step - 1))
-                else:
-                    return nums[start_index]
-            else:
-                return nums[start_index]
+            return nums[start_index]
 
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
         if len(nums) <= 1:
@@ -30,8 +19,7 @@ class Solution:
         result = float('-inf')
 
         for central_index in range(0, len(completed_array)):
-            left = self.getMaxResult(central_index, -1, completed_array, max_step)
-            right = self.getMaxResult(central_index, 1, completed_array, max_step)
-            result = max(max(left, right), result)
+            right = self.getMaxResult(central_index, completed_array, max_step)
+            result = max(right, result)
 
         return result
