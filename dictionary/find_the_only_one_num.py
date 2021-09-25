d4c65a4406fd50d31d36302bf49b0b226fb56fd4
Eric Yan <yanxuok@gmail.com>
2021-09-25 16:04:37 +0800

    another method

--- dictionary/find_the_only_one_num.py
+++ dictionary/find_the_only_one_num.py
@@ -35,3 +35,9 @@ class Solution:
                 return key
 
         return 0
+
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res = res ^ num
+        return res
